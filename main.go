6389a67bccf46c11e3683ffa843c636382b5f629
Alessandro Cappellini <a.cappellini92@gmail.com>
2020-10-15 14:45:26 +0200

    Create simple server

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home of my website!")
+}
+
+func main() {
+	http.HandleFunc("/", home) // The dafault url is localhost:8080
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
