933a1b1b385de4ffcc0f4d5a7758b5821972251d
Alessandro Cappellini <a.cappellini92@gmail.com>
2020-11-10 12:46:11 +0100

    Handle num of request for worker

--- httpload.go
+++ httpload.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 type stats struct {
@@ -11,32 +12,40 @@ type stats struct {
 	successRequest int
 }
 
-func (s *stats) req(url string) {
-	r, err := http.Get(url)
-	if err != nil {
-		s.failedRequest++
-		return
+func (s *stats) req(url string, reqNum int) {
+	for i := 0; i < reqNum; i++ {
+		r, err := http.Get(url)
+		if err != nil {
+			s.failedRequest++
+			return
+		}
+		fmt.Println(r.StatusCode)
+
+		s.successRequest++
 	}
-	fmt.Println(r.StatusCode)
-
-	s.successRequest++
-
 }
 
 func main() {
 	URL := ""
 	concNum := 0
+	reqNum := 0
 
 	flag.StringVar(&URL, "", "http://www.google.it", "url to test")
 	flag.IntVar(&concNum, "w", 50, "number of workers to run concurrently. default:50")
+	flag.IntVar(&reqNum, "n", 200, "number of requests to run. default:200.")
 	flag.Parse()
+
+	if reqNum < concNum {
+		fmt.Println("The number of request can't be smaller than number of workers")
+		os.Exit(1)
+	}
 
 	s := stats{}
 	s.successRequest = 0
 	s.failedRequest = 0
 
 	for i := 0; i < concNum; i++ {
-		go s.req(URL)
+		go s.req(URL, reqNum/concNum)
 	}
 
 	var input string
